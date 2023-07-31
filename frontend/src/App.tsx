d75e27bc733d19927937eee933df097688b01476
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-31 13:33:54 +0530

    Add buttonHandler

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const buttonHandler = async (i: number, j: number) => {
+    // Mark the user clicked cell
+    const boardCopy: Board = JSON.parse(JSON.stringify(board));
+    boardCopy[i][j] = Cell.User;
+    setBoardData({ ...boardData, board: boardCopy });
+
+    // Set loading true
+    setLoading(true);
+
+    // Compute the next move
+    try {
+      const data = await computeService.compute(board);
+      setLoading(false);
+      setBoardData(data);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
 };
 
 export default App;
