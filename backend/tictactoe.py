1ac7dc0bcb82e630cace2e826308a08588eb6a4d
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-31 17:03:05 +0530

    Format code

--- backend/tictactoe.py
+++ backend/tictactoe.py
@@ -1,50 +1,62 @@
 import random
-#it turns indices from a dict to number of a 2D list.         
+
+
+# it turns indices from a dict to number of a 2D list.
 def get_indices_from_number(number):
     return int(number/3), number % 3
 
 
 def check_winner(board, n):
-    combinations = [[0,3,6], [1,4,7], [2,5,8],[0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6], [0,1,2], [3,4,5], [6,7,8]]
+    combinations = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 3, 6], [1, 4, 7], [
+        2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]]
     for combination in combinations:
         for number in combination:
             i, j = get_indices_from_number(number)
-            if board[i][j] != n: break
-        else: return True
+            if board[i][j] != n:
+                break
+        else:
+            return True
     return False
 
 
 def check_tie(board):
     for lst in board:
-        if 0 in lst: break
-    else: return True
+        if 0 in lst:
+            break
+    else:
+        return True
     return False
-
 
 
 # to check if computer is one step close to winning
 def check_winning(board):
-    combinations = [[0,3,6], [1,4,7], [2,5,8],[0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6], [0,1,2], [3,4,5], [6,7,8]]
+    combinations = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 3, 6], [1, 4, 7], [
+        2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]]
     count = 0
-    
+
     for combination in combinations:
         for number in combination:
             i, j = get_indices_from_number(number)
             if board[i][j] == 2:
                 count += 1
-            if board[i][j] == 1: break
+            if board[i][j] == 1:
+                break
         else:
             if count == 2:
                 for number in combination:
                     i, j = get_indices_from_number(number)
-                    if board[i][j] == 0: return number
+                    if board[i][j] == 0:
+                        return number
         count = 0
     return False
 
-#to see if the player has a good spot to mark if they aren't close to winning/defending
+
+
+# to see if the player has a good spot to mark if they aren't close to winning/defending
 def next_best(board):
-    combinations = [[0,3,6], [1,4,7], [2,5,8],[0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6], [0,1,2], [3,4,5], [6,7,8]]
-    count  = 0
+    combinations = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 3, 6], [1, 4, 7], [
+        2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    count = 0
 
     for combination in combinations:
         for number in combination:
@@ -52,14 +64,17 @@ def next_best(board):
 
             if board[i][j] == 2:
                 count += 1
-            elif board[i][j] == 1: break
+            elif board[i][j] == 1:
+                break
         else:
             if count == 1:
-                return random.choice([i for i in combination if board[int(i/3)][i%3]== 0])
-#if there exists no symbol yet. creates one randomly
+                return random.choice([i for i in combination if board[int(i/3)][i % 3] == 0])
+
+    # if there exists no symbol yet. creates one randomly
     while True:
         i = random.randint(1, 10)
-        if board[int(i/3)][i%3] == None: return i
+        if board[int(i/3)][i % 3] == None:
+            return i
 
 
 # take a 3x3 2d list
@@ -83,12 +98,9 @@ def compute_board(board):
         i, j = get_indices_from_number(number)
         board[i][j] = 2
         result['is_complete'], result['winner'] = True, 2
-        
-    else: 
+    else:
         number = next_best(board)
         i, j = get_indices_from_number(number)
         board[i][j] = 2
         result['is_complete'] = False
     return result
-
-
