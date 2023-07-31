9642e73af40203b68eb73c88940539a6bccda53d
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-31 13:34:21 +0530

    Add getSummaryMessage fn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 
 const App = () => {
+
+  const getSummaryMessage = (): string => {
+    let message = "Game Over! ";
+    switch (winner) {
+      case Winner.Computer:
+        message += "You lost, better luck next time!";
+        return message;
+      case Winner.User:
+        message += "You won, congratulations!";
+        return message;
+      case Winner.Tie:
+        message += "It's a tie!";
+        return message;
+      default:
+        return assertNever(winner);
+    }
+  };
+
   const buttonHandler = async (i: number, j: number) => {
     // Mark the user clicked cell
     const boardCopy: Board = JSON.parse(JSON.stringify(board));
