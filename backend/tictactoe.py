35c6ed4b9d804c01bb1fde8ee870f33adb15d220
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-31 14:25:57 +0530

    Rename is_complete to isComplete

--- backend/tictactoe.py
+++ backend/tictactoe.py
@@ -11,7 +11,7 @@
 # winner - 0 => tie
 # winner - 1 => user
 # winner - 2 => computer
-# {board: [[]], is_complete: true/false, winner: 0/1/2}
+# {board: [[]], isComplete: true/false, winner: 0/1/2}
 def compute_board(board):
   return {"board": [[0, 0, 1], [0, 1, 0], [1, 0, 0]],
               "isComplete": True, "winner": 2}
