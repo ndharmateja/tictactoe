e42be40cd2072a259ad498e27fd53c96bf5c1573
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-31 18:27:05 +0530

    Fix random int range bug

--- backend/tictactoe.py
+++ backend/tictactoe.py
@@ -72,7 +72,7 @@ def next_best(board):
 
     # if there exists no symbol yet. creates one randomly
     while True:
-        i = random.randint(1, 10)
+        i = random.randint(0, 8)
         if board[int(i/3)][i % 3] == 0:
             return i
 
