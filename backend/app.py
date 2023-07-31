c9fa929839b3327f6cd890cfd3653617df948971
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-31 14:10:47 +0530

    Setup error handling in the server

--- backend/app.py
+++ backend/app.py
@@ -7,11 +7,24 @@ app = Flask(__name__)
 CORS(app)
 
 
+# TODO
+def validate_board(board):
+    raise TypeError("")
+    pass
+
+
 @app.route("/compute", methods=["POST"])
 def compute():
     board = request.get_json()
-    result = compute_board(board)
-    return jsonify(result), 201
+    try:
+        validate_board(board)
+        result = compute_board(board)
+        return jsonify(result), 201
+    except TypeError as e:
+        return jsonify({"error": str(e)}), 401
+    except:
+        return jsonify({"error": "Unknown error"}), 500
+
 
 
 if __name__ == "__main__":
