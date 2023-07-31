45b56130270dd08ff333a15771677fab6ebe758a
Bindu Hegde <binduhegde1932@gmail.com>
2023-07-31 16:12:48 +0530

    finish validation

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 CORS(app)
 
 
-# TODO
 # # Validations:
 # 1. Every cell should be a number and between 0 and 2
 # 2. Number of 1s should be greater than number of 2s by 1
@@ -19,8 +18,22 @@ def validate_board(board):
     for row in board:
         if len(row) != 3:
             raise TypeError("each row in board needs to have 3 elements")
+        
+    num_ones=0
+    num_twos=0
+    for row in board:
+        for number in row:
+            if type(number) != int:
+                raise TypeError("each element needs to be an integer")
+            if number < 0 or number > 2:
+                raise TypeError("each element needs to be an integer")
+            if number == 0:
+                num_ones += 1
+            elif number == 1:
+                num_twos += 1
 
-    
+    if num_ones-num_twos != 1:
+        raise TypeError("invalid number of ones and twos")
 
 
 @app.route("/compute", methods=["POST"])
