fd57005bb532205ac10faf7ca5e5e7c601fac579
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-08-01 17:40:50 +0530

    Update api url

--- frontend/src/service.ts
+++ frontend/src/service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Board, BoardData } from "./types";
 
-const baseUrl = "https://ndharmateja.pythonanywhere.com";
+const baseUrl = "https://apitictactoe.pythonanywhere.com";
 
 const compute = async (board: Board): Promise<BoardData> => {
   const { data } = await axios.post<BoardData>(`${baseUrl}/compute`, board);
